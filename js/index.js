06547a77002c68a20ca51fa2ab67d090e4dc8f06
MoNgc89 <mgalindo.cruz@gmail.com>
2019-04-04 21:30:39 -0600

    segundo commit con js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var d = document.getElementById('dibujis')
+var lienzo = d.getContext('2d')
+
+dibujarLineas('hotpink', 15, 30, 200, 150)
+dibujarLineas('gold', 30, 60, 100, 50)
+dibujarLineas('aliceblue', 205, 130, 30, 15)
+
+
+function dibujarLineas(color, xinicial, yinicial, xfinal, yfinal){
+  lienzo.beginPath()
+  lienzo.strokeStyle = color
+  lienzo.moveTo(xinicial, yinicial)
+  lienzo.lineTo(xfinal, yfinal)
+  lienzo.stroke()
+  lienzo.closePath()
+}
